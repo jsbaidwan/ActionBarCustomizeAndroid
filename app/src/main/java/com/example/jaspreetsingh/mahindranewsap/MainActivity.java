b40c06904b4a07daf0924daa142ff7cf4451cf3f
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-03-14 23:08:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jaspreetsingh/mahindranewsap/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.jaspreetsingh.mahindranewsap;
+
+import android.app.ActionBar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
