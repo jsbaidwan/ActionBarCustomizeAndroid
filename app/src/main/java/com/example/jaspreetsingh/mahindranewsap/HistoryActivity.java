dffedc2af63391e0ab4dc91d378837dc984189d1
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-03-17 12:11:03 +0530

    ADD: style to edit box, create two activities

--- app/src/main/java/com/example/jaspreetsingh/mahindranewsap/HistoryActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/mahindranewsap/HistoryActivity.java
@@ -1,8 +1,20 @@
 package com.example.jaspreetsingh.mahindranewsap;
 
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by jaspreet.singh on 3/16/2018.
  */
 
-public class HistoryActivity {
+public class HistoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        //getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+    }
 }
--- app/src/main/java/com/example/jaspreetsingh/mahindranewsap/HomeActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/mahindranewsap/HomeActivity.java
@@ -1,9 +1,20 @@
 package com.example.jaspreetsingh.mahindranewsap;
 
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by jaspreet.singh on 3/15/2018.
  */
 
-public class HomeActivity {
-
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+    }
 }
